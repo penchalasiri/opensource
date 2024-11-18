08ee427a07dca4e5baa91c46f94cc04cd54280fb
penchalasiri <penchalasiri56@gmail.com>
2024-11-18 16:16:15 +0530

    Create RPS.py

---
+++ visaa_prep/RPS.py
@@ -0,0 +1,7 @@
+V,C=input().split()
+if (V== "R" and C=="S") or (V=="S" and C == "P") or (V == "P" and C == "R"):
+    print("Vignesh")
+elif (C=="R" and V=="S") or (C=="S" and V == "P") or (C == "P" and V == "R"):
+      print("Charan")
+else:
+      print("NULL")
